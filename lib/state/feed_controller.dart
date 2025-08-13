56860ff0ac79a1afdb0d6490ffb00da680d74e0c
imattau <imatt.au@protonmail.com>
2025-08-13 20:47:12 +1000

    feat: add vertical swipe feed with mock posts

--- lib/state/feed_controller.dart
+++ lib/state/feed_controller.dart
@@ -1,1 +1,39 @@
-// TODO: feed state controller
+import 'package:flutter/foundation.dart';
+import '../data/models/post.dart';
+import '../data/repos/feed_repository.dart';
+
+/// Minimal controller. Replace with Riverpod later if desired.
+class FeedController extends ChangeNotifier {
+  final FeedRepository repo;
+  FeedController(this.repo);
+
+  final List<Post> _posts = [];
+  int _index = 0;
+
+  List<Post> get posts => List.unmodifiable(_posts);
+  int get index => _index;
+
+  /// For tests and UI hints: indices we want to preload (±1)
+  Set<int> get preloadCandidates {
+    final s = <int>{};
+    if (_posts.isEmpty) return s;
+    if (_index - 1 >= 0) s.add(_index - 1);
+    if (_index + 1 < _posts.length) s.add(_index + 1);
+    return s;
+  }
+
+  Future<void> loadInitial() async {
+    final data = await repo.fetchInitial();
+    _posts
+      ..clear()
+      ..addAll(data);
+    _index = 0;
+    notifyListeners();
+  }
+
+  void onPageChanged(int i) {
+    if (i == _index) return;
+    _index = i;
+    notifyListeners();
+  }
+}
--- lib/ui/home/widgets/video_player_view.dart
+++ lib/ui/home/widgets/video_player_view.dart
@@ -1,10 +1,58 @@
 import 'package:flutter/material.dart';
+import '../../../state/feed_controller.dart';
+import '../../../data/repos/feed_repository.dart';
+import '../../../data/models/post.dart';
+import 'video_card.dart';
 
-class VideoPlayerView extends StatelessWidget {
+class VideoPlayerView extends StatefulWidget {
   const VideoPlayerView({super.key});
+
+  @override
+  State<VideoPlayerView> createState() => _VideoPlayerViewState();
+}
+
+class _VideoPlayerViewState extends State<VideoPlayerView> {
+  late final FeedController controller;
+  final PageController pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller = FeedController(MockFeedRepository());
+    controller.addListener(_onController);
+    controller.loadInitial();
+  }
+
+  void _onController() {
+    if (mounted) setState(() {});
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(_onController);
+    pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // Placeholder; real PageView and players arrive in PR 3
-    return Container(color: Colors.black);
+    final posts = controller.posts;
+    if (posts.isEmpty) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    return PageView.builder(
+      controller: pageController,
+      scrollDirection: Axis.vertical,
+      onPageChanged: controller.onPageChanged,
+      itemCount: posts.length,
+      itemBuilder: (context, i) {
+        final Post p = posts[i];
+        final isCurrent = i == controller.index;
+        final isNeighbour = controller.preloadCandidates.contains(i);
+        return VideoCard(
+            post: p, isCurrent: isCurrent, isNeighbour: isNeighbour);
+      },
+    );
   }
 }
