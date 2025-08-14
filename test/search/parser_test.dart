f07ecaa4b274e2e2803f376ec72acc800d0fb538
imattau <imatt.au@protonmail.com>
2025-08-15 06:29:38 +1000

    test: use valid npub in parser test

--- test/search/parser_test.dart
+++ test/search/parser_test.dart
@@ -8,7 +8,12 @@ void main() {
   });
   test('parses author', () {
     expect(SearchParser.parseAuthor('f'.padLeft(64, 'a'))!.length, 64);
-    expect(SearchParser.parseAuthor('npub1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq'), isNotNull);
+    expect(
+      SearchParser.parseAuthor(
+        'npub180cvv07tjdrrgpa0j7j7tmnyl2yr6yr7l8j4s3evf6u64th6gkwsyjh6w6',
+      ),
+      '3bf0c63fcb93463407af97a5e5ee64fa883d107ef9e558472c4eb9aaaefa459d',
+    );
   });
   test('parses text', () {
     expect(SearchParser.parseText('  hello world '), 'hello world');
