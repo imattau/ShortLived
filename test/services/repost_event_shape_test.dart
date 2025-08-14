f2876ad938bfb75cbe42755e876c5a7fc5a8b350
imattau <imatt.au@protonmail.com>
2025-08-15 06:42:07 +1000

    Add priv constant in repost event test

--- test/services/repost_event_shape_test.dart
+++ test/services/repost_event_shape_test.dart
@@ -4,6 +4,8 @@ import 'package:nostr_video/crypto/nostr_event.dart';
 void main() {
   test('kind 6 repost event canonical id is deterministic', () {
     const pub = '02' 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+    const priv = '11' '11' '11' '11' '11' '11' '11' '11' '11' '11' '11' '11' '11' '11' '11' '11'
+        '11' '11' '11' '11' '11' '11' '11' '11' '11' '11' '11' '11' '11' '11' '11' '11';
     final e = NostrEvent(kind: 6, createdAt: 1700000000, content: '', tags: const [["e","evt1"]]);
     final idA = NostrEvent.idFor(pub, e);
     final idB = NostrEvent.idFor(pub, e);
