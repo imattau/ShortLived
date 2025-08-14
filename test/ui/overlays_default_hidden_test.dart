1055033dce9023857c3f454cf04db7c29f06451b
imattau <imatt.au@protonmail.com>
2025-08-14 19:30:25 +1000

    test: add shared test services

--- test/ui/overlays_default_hidden_test.dart
+++ test/ui/overlays_default_hidden_test.dart
@@ -1,22 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:nostr_video/ui/home/home_feed_page.dart';
 import '../test_utils/fake_video_player_platform.dart';
-import 'package:nostr_video/services/settings/settings_service.dart';
-import 'package:nostr_video/services/queue/action_queue_memory.dart';
-import 'package:nostr_video/services/queue/action_queue.dart';
-import 'package:nostr_video/core/di/locator.dart';
+import '../test_utils/test_services.dart';
 import 'package:network_image_mock/network_image_mock.dart';
 
 void main() {
   setUpAll(() async {
     TestWidgetsFlutterBinding.ensureInitialized();
     FakeVideoPlayerPlatform.register();
-    SharedPreferences.setMockInitialValues({'overlays_default_hidden': true});
-    final sp = await SharedPreferences.getInstance();
-    Locator.I.put<SettingsService>(SettingsService(sp));
-    Locator.I.put<ActionQueue>(ActionQueueMemory());
+    await setupTestLocator(prefs: {'overlays_default_hidden': true});
   });
   testWidgets('overlays are hidden on open when setting is true', (
     tester,
