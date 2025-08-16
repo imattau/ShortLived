f56fb9bb39dbd0aca138d44a1305e8de7e33dddd
imattau <imatt.au@protonmail.com>
2025-08-17 07:50:43 +1000

    chore: remove unused import

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/foundation.dart';
-
 /// Build flag: flutter run -d chrome --dart-define=NOSTR_ENABLED=true
 const bool kNostrEnabled = bool.fromEnvironment('NOSTR_ENABLED', defaultValue: false);
 
