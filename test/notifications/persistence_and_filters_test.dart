d146e03f8df56422002d556c120e2318fd94424c
imattau <imatt.au@protonmail.com>
2025-08-16 10:23:03 +1000

    chore: satisfy analyzer warnings

--- test/notifications/persistence_and_filters_test.dart
+++ test/notifications/persistence_and_filters_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:nostr_video/notifications/notification_models.dart';
 import 'package:nostr_video/notifications/notifications_store.dart';
