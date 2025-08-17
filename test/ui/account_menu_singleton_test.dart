7e3bb7e735b2cb177e1a70dc673ae445e5f22c9a
imattau <imatt.au@protonmail.com>
2025-08-17 14:11:51 +1000

    test: wrap account menu test with video scope

--- test/ui/account_menu_singleton_test.dart
+++ test/ui/account_menu_singleton_test.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nostr_video/ui/home/home_page.dart';
 import 'package:nostr_video/ui/overlay/widgets/viewer_avatar.dart';
+import '../test_helpers/test_video_scope.dart';
 
 void main() {
   testWidgets('avatar toggles single account menu instance', (t) async {
-    await t.pumpWidget(const MaterialApp(home: HomePage()));
+    await t
+        .pumpWidget(const TestVideoApp(child: MaterialApp(home: HomePage())));
     await t.pumpAndSettle();
 
     final avatar = find.byType(ViewerAvatar);
